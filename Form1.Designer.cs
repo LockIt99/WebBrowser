77348c969f057b25c23b51595f5bb92aae51924a
LockIt99 <kishorekummitha@gmail.com>
2023-01-30 18:28:30 +0530

    ID#5099 WebBrowser - Adding SuppressScriptErrors

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WebBrowser
+{
+    partial class WinForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.cbURL = new System.Windows.Forms.ToolStripComboBox();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.btnForward = new System.Windows.Forms.ToolStripButton();
+            this.btnReload = new System.Windows.Forms.ToolStripButton();
+            this.btnGo = new System.Windows.Forms.ToolStripButton();
+            this.btnBack = new System.Windows.Forms.ToolStripButton();
+            this.cms1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnBack,
+            this.btnForward,
+            this.btnReload,
+            this.toolStripSeparator1,
+            this.cbURL,
+            this.btnGo});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 34);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 34);
+            // 
+            // cbURL
+            // 
+            this.cbURL.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+            this.cbURL.DropDownWidth = 200;
+            this.cbURL.Name = "cbURL";
+            this.cbURL.Size = new System.Drawing.Size(200, 34);
+            this.cbURL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cbURL_KeyDown);
+            // 
+            // browser
+            // 
+            this.browser.ContextMenuStrip = this.cms1;
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 34);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(800, 416);
+            this.browser.TabIndex = 1;
+            this.browser.TabStop = false;
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Image = global::WebBrowser.Properties.Resources.forward1;
+            this.btnForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(105, 29);
+            this.btnForward.Text = "Forward";
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Image = global::WebBrowser.Properties.Resources.reload;
+            this.btnReload.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(94, 29);
+            this.btnReload.Text = "Reload";
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnGo.Image = global::WebBrowser.Properties.Resources.go;
+            this.btnGo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(34, 29);
+            this.btnGo.Text = "Go";
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Image = global::WebBrowser.Properties.Resources.back;
+            this.btnBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(76, 29);
+            this.btnBack.Text = "Back";
+            this.btnBack.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // cms1
+            // 
+            this.cms1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.cms1.Name = "cms1";
+            this.cms1.Size = new System.Drawing.Size(241, 37);
+            this.cms1.Text = "Kishore";
+            // 
+            // WinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WinForm";
+            this.Text = "Brave";
+            this.Load += new System.EventHandler(this.WinForm_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.ToolStripButton btnBack;
+        private System.Windows.Forms.ToolStripButton btnForward;
+        private System.Windows.Forms.ToolStripButton btnReload;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripComboBox cbURL;
+        private System.Windows.Forms.ToolStripButton btnGo;
+        private System.Windows.Forms.ContextMenuStrip cms1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WebBrowser
+{
+    public partial class WinForm : Form
+    {
+        public WinForm()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            browser.GoBack();
+        }
+
+        private void cbURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(cbURL.Text.Length > 0 && e.KeyCode == Keys.Enter) 
+            {
+                browser.Navigate(cbURL.Text);
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            browser.GoForward();
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            browser.Refresh();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            browser.Navigate(cbURL.Text);
+        }
+
+        private void WinForm_Load(object sender, EventArgs e)
+        {
+            browser.ScriptErrorsSuppressed = true;
+            string appName =Process.GetCurrentProcess().ProcessName + ".exe";
+            //MessageBox.Show("Current process name is " + appName);
+           SetIEkeyBrowserContrl(appName);
+        }
+
+      static void SetIEkeyBrowserContrl(string appName)
+        {
+            RegistryKey regKey = null;
+            try
+            {
+                if (Environment.Is64BitOperatingSystem)
+                    regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Wow6432Node\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                else
+                    regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                if(regKey == null)
+                {
+                    MessageBox.Show("Failed to save settings. Address not found");
+                    return;
+                }
+                string key = regKey.GetValue(appName).ToString();
+               // MessageBox.Show($"Available Key for {appName}.{key}");
+                if (key == "8000")
+                {
+                    MessageBox.Show("It's already there.");
+                    regKey.Close();
+                    return;
+                }
+                else if (string.IsNullOrEmpty(key))
+                {
+                    regKey.SetValue(appName, unchecked((int)0x1F40),RegistryValueKind.DWord);
+                }
+                //key = regKey.GetValue(appName).ToString();
+                //if (key == "8000")
+                //    MessageBox.Show("Application Settings Applied Successfully");
+                //else
+                //    MessageBox.Show("Application Settings Failed, Ref: " + key);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured while doing reg setup -  "+ ex.Message);
+                
+            }
+            finally
+            {
+                if(regKey != null) regKey.Close();
+            }
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Text = "Brave - " + browser.Document.Title;
+        }
+    }
+}
